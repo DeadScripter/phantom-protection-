59a1b6650aa829c4f6199c1b87e2084022bc000d
DEAD <hellowassim86@gmail.com>
2026-01-06 16:59:48 +0100

    Create cors.ts

---
+++ backend/supabase/functions/_shared/cors.ts
@@ -0,0 +1,5 @@
+export const CORS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'authorization, content-type, x-client-info',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+}
