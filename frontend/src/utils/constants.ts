3a7fa9ab3b7e5f0319b6c99f368c0e6500f8ad46
DEAD <hellowassim86@gmail.com>
2026-01-06 16:36:55 +0100

    Create constants.ts

---
+++ frontend/src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || ''
+export const SUPABASE_ANON = import.meta.env.VITE_SUPABASE_ANON || ''
+export const SITE_NAME = 'Phantom Protection'
