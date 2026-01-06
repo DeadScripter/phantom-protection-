1cababa0d7e4ba06a59073eeb2f18f16d24858cc
DEAD <hellowassim86@gmail.com>
2026-01-06 22:32:52 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/home.'
+import Home from './pages/home'
 import Dashboard from './pages/Dashboard'
 import NotFound from './pages/NotFound'
 import { AuthGuard } from './components/Auth/AuthGuard'
